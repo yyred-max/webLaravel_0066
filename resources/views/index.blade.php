3b99ccc418c93836776e733d9a6bb6819b784161
yyred-max <anay29758@gmail.com>
2026-07-11 15:47:53 +0700

    Membuat Hero Section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
 @section('body')
 
 <!-- Navbar -->
- <nav class="navbar navbar-expand-lg navbar-news">
+<nav class="navbar navbar-expand-lg navbar-news">
     <div class="container">
         <a class="navbar-brand" href="#">Kabar Burung</a>
         <buton class="navbar-toggler" type="button" data-bs-toogle="collapse" data-bs-target="#navbarNews">
@@ -51,7 +51,20 @@
  </nav>
 
 <!-- Hero -->
+<section class="cotainer">
+    <div class="hero-section" style="background-image: url('images/kota-semarang.jpeg');">
+        <div class="hero-overlay">
+            <div class="hero-meta-row">
+                <span class="badge-utama">LAPORAN UTAMA</span>
+                <span class="hero-meta">12 Menit Lalu &bull; Oleh Redaksi</span>
+            </div>
+        <h1 class="hero-title">Masa Depan Transformasi Digital Nasional Menuju 2045</h1>
+        <p class="hero-desc">Visi Indonesia Emas 2045 menuntut percepatan integrasi teknologi di seluruh lini pemerintahan dan pelayanan publik guna mencapai efisiensi maksimal.</p>
+        <a href="#" class="btn-hero">Baca Selengkapnya <i class="bi bi-arrow-right"></i></a>
+        </div>
+    </div>
 
+</section>
 
 <!-- Berita Terbaru -->
 
